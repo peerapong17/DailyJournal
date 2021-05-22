725788f8639bb60597a221a499aa5bd74c5312aa
peerapong17 <17peerapong4.0@gmail.com>
2021-05-22 22:27:45 +0700

    DailyJournal

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -4,85 +4,123 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const app = express();
 
-let posts = [];
-
 app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride('_method'))
 
-app.get("/", function(req, res){
-  res.render("home", {home: homeStartingContent, posts: posts});
-});
+mongoose.connect("mongodb://localhost:27017/newsDB", { useNewUrlParser: true })
 
-app.get("/about", function(req, res){
-  res.render("about", {about: aboutContent});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contact: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-
-  const { postTitle, postContent } = req.body
-
-  const composeObject = {
-    titleBody: postTitle,
-    contentBody: postContent
-  }
-
-  posts.push(composeObject)
-
-  res.redirect("/")
-});
+const newsSchema = {
+  title: String,
+  content: String,
+  category: String
+}
 
 
-app.get("/posts/:topic", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.topic)
+const News = mongoose.model('New', newsSchema)
 
-  posts.map(function(post){
-    const storedTitle = _.lowerCase(post.titleBody)
+const categories = ["sports", "entertainment", "health", "business", "technology"];
 
-    if (requestedTitle === storedTitle){  
-      res.render("post", {
-        Title: post.titleBody,
-        Content: post.contentBody
-      });
+app.get("/", function (req, res) {
+  News.find(function (err, foundPosts) {
+    if (!err) {
+      res.render('home', { posts: foundPosts })
+    } else {
+      res.send(err)
     }
   })
 });
 
-app.listen(8080, function() {
-  console.log("Server started on port 3000");
+app.get("/contact", function (req, res) {
+  res.render("contact", { contact: contactContent });
 });
 
+app.route("/compose")
+  .get(function (req, res) {
+    res.render("compose", { categories });
+  })
+  .post(function (req, res) {
 
+    const { postTitle, postContent, postCategory } = req.body
 
+    const news = new News({
+      title: postTitle,
+      content: postContent,
+      category: postCategory
+    })
 
+    news.save()
 
+    res.redirect("/")
+  });
 
+app.route("/compose/:id")
+  .get(function (req, res) {
+    const { id } = req.params
 
+    News.findById(id, function (err, foundItems) {
+      if (!err) {
+        res.render("updatepost", {
+          Items: foundItems,
+          categories
+        });
+      } else {
+        res.send(err)
+      }
+    })
+  })
+  .patch(function (req, res) {
+    const { id } = req.params
+    News.findByIdAndUpdate(id, req.body, function (err) {
+      if (!err) {
+        res.redirect("/")
+      } else {
+        res.send(err)
+      }
+    })
+  })
+  .delete(function (req, res) {
+    const { id } = req.params
+    console.log(id)
+    News.findByIdAndDelete(id, function (err) {
+      if (!err) {
+        res.redirect("/")
+      } else {
+        res.send(err)
+      }
+    })
+  });
 
+app.get("/posts/:id", function (req, res) {
+  const { id } = req.params
 
+  News.findById(id, function (err, foundItems) {
+    if (!err) {
+      res.render("detail", {
+        Items: foundItems
+      });
+    } else {
+      res.send(err)
+    }
+  })
+});
 
+app.get("/:topic", function (req, res) {
+  const { topic } = req.params
+  News.find({ category: topic }, function (err, foundTopics) {
+    if (!err) {
+      res.render("home", { posts: foundTopics })
+    } else {
+      res.send(err)
+    }
+  })
+});
 
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(8080, function () {
   console.log("Server started on port 3000");
 });
